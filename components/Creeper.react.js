d6b8680012ee5fd682b9621a497f2f8710e76c33
remyyounes <ryounes@gmail.com>
2014-12-14 23:05:44 -0800

    f

--- components/Creeper.react.js
+++ components/Creeper.react.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 var React = require("react");
 var FileDropper = require("./FileDropper.react");
+var EmailCollection = require('./EmailCollection.react');
 var Creeper = React.createClass({
   render: function() {
     return (
