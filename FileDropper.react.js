18f0369ca3966c5a871702c75a787cd210db8907
remyyounes <ryounes@gmail.com>
2014-12-14 22:55:47 -0800

    wowz

--- FileDropper.react.js
+++ FileDropper.react.js
@@ -5,9 +5,18 @@ var FileDropper = React.createClass({
     return (
       <div className="file-dropper">
         <input type="file"/>
-        <div className="dropzone"/>
+        <div className="dropzone" onDrop={this.handleDrop} />
       </div>
     );
+  },
+  handleDrop: function(event) {
+    var file = event.dataTransfer.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onLoad = function () {
+      var data = reader.result;
+      console.log(data);
+    };
   }
 });
 
