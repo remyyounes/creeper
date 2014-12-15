0b5d48d909344892de5d4ed298b25d7a680d148b
remyyounes <ryounes@gmail.com>
2014-12-14 23:03:44 -0800

    JSX

--- components/Creeper.react.js
+++ components/Creeper.react.js
@@ -1,3 +1,4 @@
+/** @jsx React.DOM */
 var React = require("react");
 var FileDropper = require("./FileDropper.react");
 var Creeper = React.createClass({
--- components/FileDropper.react.js
+++ components/FileDropper.react.js
@@ -1,3 +1,4 @@
+/** @jsx React.DOM */
 var React = require("react");
 
 var FileDropper = React.createClass({
